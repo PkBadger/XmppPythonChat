6599484ce2811f8c692ec3a126e9d6b7abbdbc7c
Francisco Tejón <tejon@BadgerDebian>
2016-05-12 10:47:18 -0600

    Revisado seguridad

--- ejemplo.py
+++ ejemplo.py
@@ -250,7 +250,7 @@ class Connect:
 
         # Connect to the XMPP server and start processing XMPP stanzas.
 
-        if xmpp.connect(address=("10.32.70.14",5222),reattempt=False,use_tls=False,use_ssl=False):
+        if xmpp.connect(reattempt=False,use_tls=False):
             # If you do not have the dnspython library installed, you will need
             # to manually specify the name of the server if it does not match
             # the one in the JID. For example, to use Google Talk you would
